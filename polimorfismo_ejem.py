661c55a8aa5d75ccc4efc5a84a76ee65e02cf0bc
Luna_Hurtado06 <hurtadoluna898@gmail.com>
2025-10-24 18:44:09 -0500

    poo

---
+++ polimorfismo_ejem.py
@@ -0,0 +1,22 @@
+#clase base
+class Animal:
+    def hablar (self):
+        print("los animales hacen un ruido")
+
+#clase hija 1
+class Perro (Animal):
+    def hablar(self):
+        print("el perro dice; Guuuaa guuuaa")
+
+#clase hija 2
+class Gato (Animal):
+    def hablar(self):
+        print("El gato dice; miau miau")
+
+#funcion que muestra el polimorfismo
+def hacer_hablar(animal):
+    animal.hablar() 
+#crear el objeto
+perro = Perro()
+gato = Gato()
+
---
+++ taller_poo.py
@@ -0,0 +1,55 @@
+#sistemas de notas
+
+# class Estudiantes:
+
+#     def __init__(self, nombre, grado, nota):
+#        self.nombre = nombre
+#        self.grado = grado
+#        self.nota = nota
+
+#     def mostrar_info(self):
+#         print(f"hola, {self.nombre} :) ")
+#         print(f"Su grado es: {self.grado}")
+#         print(f"esta fue su nota: {self.nota}")
+
+#         if self.nota >= 3.0:
+#             print("usted ha aprobado")
+#         else:
+#             print("usted reprobo")
+
+# nombre= input("Ingrese su nombre: ")
+# grado= input("Ingrese su grado: ")
+# nota= float(input("Ingrese su nota:"))
+
+# info= Estudiantes(nombre, grado, nota)
+# info.mostrar_info()
+
+# Sistema de votacion 
+class Votacion:
+    
+    def __init__(self, nombre_completo, edad, nacionalidad):
+        self.nombre_completo = nombre_completo
+        self.edad = edad
+        self.nacionalidad = nacionalidad
+
+    def mostrar_info(self):
+        print(f"nombre completo: {self.nombre_completo}")
+        print(f"edad: {self.edad}")
+        print(f"nacionalidad: {self.nacionalidad}")
+
+        if self.edad >= 18 and self.nacionalidad == "colombiano":
+            print("usted puede votar")
+        else:
+            print("usted no puede votar")
+
+nombre_completo= input("ingrese su nombre completo:")
+edad= int(input("Ingrese su edad: "))
+nacionalidad= input("ingrese su nacionalidad: ")
+
+usu= Votacion (nombre_completo, edad, nacionalidad)
+usu.mostrar_info()
+
+
+   
+
+
